ba9ea70df8648c500186ec7c01a2e50dcc8c65f0
sboli <bolivar.stephen@gmail.com>
2011-08-24 16:25:06 -0400

    Remove useless translation

--- twmnd/main.cpp
+++ twmnd/main.cpp
@@ -40,10 +40,6 @@ int main(int argc, char *argv[])
     QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
     QTextCodec::setCodecForTr(QTextCodec::codecForName("UTF-8"));
     QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
-    const QString locale = QLocale::system().name().section('_', 0, 0);
-    QTranslator translator;
-    translator.load("qt_" + locale, QLibraryInfo::location(QLibraryInfo::TranslationsPath));
-    a.installTranslator(&translator);
     Widget w;
     return a.exec();
 }
